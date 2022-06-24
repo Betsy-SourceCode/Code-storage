7856aefa415bc1b0c4b7228cf1cdde7ff9e7950f
KYH <1450392936@qq.com>
2022-06-24 13:26:56 +0800

    6-24曾

---
+++ 新的代码/MIS_CertificationApplication多版本/6-24旷/MIS_CertificationApplication/Models/Others.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS_CertificationApplication.Models
+{
+    public class Others
+    {
+        public int Value { get; set; }
+        public int ID { get; set; }
+        public int ReturnResultNum { get; set; }
+        public int CMSerial { get; set; }
+        public int CPSerial { get; set; }
+        public string Name { get; set; }
+        public string K3Parts { get; set; }
+        public string Text { get; set; }
+        public string ApplicationRef { get; set; }
+    }
+}
